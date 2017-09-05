9fda4f091ed1f40b211282dc1df2bcaa17ef1459
vinithanatarajan <vinniitha.k.n@gmail.com>
2017-09-05 23:19:26 +1200

    Added formspree.

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,8 +18,9 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "https://formspree.io/vinitha@vconnectabroad.in",
+                method: "POST",
+                dataType: "json"
                 data: {
                     name: name,
                     phone: phone,
